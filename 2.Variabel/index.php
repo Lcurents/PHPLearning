da7cb7357028717e4afd1f583e7c47e0d45a0742
Lcurents <laurentiusdika28@gmail.com>
2023-12-17 18:37:54 +0700

    step 2

---
+++ 2.Variabel/index.php
@@ -0,0 +1,16 @@
+<?php
+    // String representation
+    $nama="Dika";
+
+    // Int representation
+    $umur= 19;
+
+    // Float representation
+    $IPK=3.74;
+
+    // Boolean representation
+    $lulus=True;
+
+    // Penggunaan
+    echo"Halo gaes nama saya {$nama} dan umurku {$umur}. di kuliah IPK saya {$IPK} dan saya dinyatakan {$lulus}";
+?>
