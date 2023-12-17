52c031d046fcc060abe61e10c0c903934343a0fc
Lcurents <laurentiusdika28@gmail.com>
2023-12-17 18:13:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    echo"halo namaku Dika,kalo kamu siapa? <br>";
+    echo"Kalau umur?";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "MAntap";
+    ?>
+    <button>Keluar</button>
+</body>
+</html>
